354e779ccb8e3fb46c24667bca79b33a60d0b348
Andrei Churyla <AndrewChuriloMinsk@gmail.com>
2019-04-09 22:22:43 +0300

    Add TTS

--- app/src/main/java/com/example/texttospeachexample/MainActivity.java
+++ app/src/main/java/com/example/texttospeachexample/MainActivity.java
@@ -5,14 +5,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
     private ArrayList<String> data;
     private RecyclerView mRecyclerView;
     private MainAdapter mMainAdapter;
+    private TextToSpeech tts;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setAdapter(mMainAdapter);
         mRecyclerView.setLayoutManager(linearLayoutManager);
+        initTts();
     }
 
     private void initData() {
@@ -31,6 +39,59 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 1; i < 11; i++) {
             String text = "2 X " + i + " = " + i * 2;
             data.add(text);
+        }
+    }
+
+    private void initTts() {
+        tts = new TextToSpeech(this, this);
+        tts.setLanguage(Locale.US);
+        tts.setOnUtteranceProgressListener(new MyListener());
+    }
+
+    @Override
+    public void onInit(int status) {
+        playSound(0);
+    }
+
+    private void playSound(int index) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, String.valueOf(index));
+        tts.speak(data.get(index), TextToSpeech.QUEUE_ADD, hashMap);
+    }
+
+    class MyListener extends UtteranceProgressListener {
+        @Override
+        public void onStart(String utteranceId) {
+            System.out.println("onStart");
+            int currentIndex = Integer.parseInt(utteranceId);
+            mMainAdapter.setCurrentPosition(currentIndex);
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mMainAdapter.notifyDataSetChanged();
+                }
+            });
+        }
+
+        @Override
+        public void onDone(String utteranceId) {
+            System.out.println("onDone");
+            int currentIndex = Integer.parseInt(utteranceId);
+            mMainAdapter.setCurrentPosition(-1);
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mMainAdapter.notifyDataSetChanged();
+                }
+            });
+            if (currentIndex < data.size() - 1) {
+                playSound(currentIndex + 1);
+            }
+        }
+
+        @Override
+        public void onError(String utteranceId) {
+            System.out.println("onError");
         }
     }
 }
